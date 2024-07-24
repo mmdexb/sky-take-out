170f636939cfb7a48dc7bfb91edff9e3e6c99c02
LinusChen <1367072295@qq.com>
2024-07-24 22:16:23 +0800

    feat(admin-employee): add page query and status update APIs
    Add new APIs in EmployeeController for employee page query and status update
    operations. Modify EmployeeMapper and EmployeeService accordingly to support
    the new functionalities. Also, update the WebMvcConfiguration for custom
    message converter configuration.
    
    BREAKING CHANGE: The EmployeeMapper interface and related XML mappings have
    been updated to include new queries and updates. Make sure to update your
    database schema and any existing code that depends on EmployeeMapper.

--- sky-server/src/main/java/com/sky/controller/admin/EmployeeController.java
+++ sky-server/src/main/java/com/sky/controller/admin/EmployeeController.java
@@ -3,18 +3,17 @@ package com.sky.controller.admin;
 import com.sky.constant.JwtClaimsConstant;
 import com.sky.dto.EmployeeDTO;
 import com.sky.dto.EmployeeLoginDTO;
+import com.sky.dto.EmployeePageQueryDTO;
 import com.sky.entity.Employee;
 import com.sky.properties.JwtProperties;
+import com.sky.result.PageResult;
 import com.sky.result.Result;
 import com.sky.service.EmployeeService;
 import com.sky.utils.JwtUtil;
 import com.sky.vo.EmployeeLoginVO;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -78,6 +77,33 @@ public class EmployeeController {
         employeeService.save(employeeDTO);
         return Result.success();
 
+    }
+    @GetMapping("/page")
+    public Result<PageResult> page(EmployeePageQueryDTO employeePageQueryDTO){
+        log.info("分页查询员工信息,参数为：{}",employeePageQueryDTO);
+        PageResult pageResult = employeeService.pageQuery(employeePageQueryDTO);
+        return Result.success(pageResult);
+    }
+
+    @PostMapping("/status/{status}")
+    public Result startOrstop(@PathVariable Integer status,Long id){
+        log.info("启用禁用员工账号,参数为：{},{}",status,id);
+        employeeService.startOrstop(status,id);
+        return  Result.success();
+    }
+
+    @GetMapping("/{id}")
+    public Result<Employee> getById(@PathVariable Long id){
+        log.info("根据id查询员工信息,参数为：{}",id);
+        Employee employee = employeeService.getById(id);
+        return Result.success(employee);
+    }
+
+    @PutMapping
+    public Result update(@RequestBody EmployeeDTO employeeDTO){
+        log.info("修改员工信息,参数为：{}",employeeDTO);
+        employeeService.update(employeeDTO);
+        return Result.success();
     }
 
 }
